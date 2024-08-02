21d39f8fa13b5213be6f9b1c20bc51246297039d
Chris Zambito <c.r.zambito@gmail.com>
2024-08-02 18:00:45 -0400

    fix static adapter bug

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-		})
-	},
-	preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter({
+            pages: "build",
+            assets: "build",
+            fallback: "index.html",
+        }),
+    },
+    preprocess: vitePreprocess(),
 };
 
 export default config;
