6d28709449625a989a79f59b65d97f3ae36b460a
JSantana11b <133810363+JSantana11b@users.noreply.github.com>
2024-08-02 12:48:54 -0400

    3rd commit.3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
 <svelte:head>
   <title>Eat Smart</title>
   <meta name="description" content="Barcode scanner for ingredients" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
+<script>
+  import { goto } from "$app/navigation";
+  const aboutUsButton = () => {
+    goto("/about-us");
+  };
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-Hello world
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<div class="">
+  <button
+    on:click={aboutUsButton}
+    class="bg-red-500 px-6 py-4 rounded-xl text-w">Hello world</button
+  >
+</div>
+
+<style>
+  .text-w {
+    color: white;
+  }
+</style>
