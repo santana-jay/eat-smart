7b4e875a1c7b9127a672fee7cef597e84978c88f
Chris Zambito <c.r.zambito@gmail.com>
2024-08-02 20:13:29 -0400

    updated logos, added favicon, new footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
-  import TopNav from "./navigation/home-page/TopNav.svelte";
+  import TopNav from "./navigation/home-page/components/TopNav.svelte";
+  import Footer from "./navigation/home-page/components/Footer.svelte";
 </script>
 
 <main>
   <TopNav />
+  <div class="p-40"></div>
+  <Footer />
 </main>
