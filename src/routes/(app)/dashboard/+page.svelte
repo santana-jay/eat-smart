1a2c3b68ac98b0abf6a377e26e9727c26a82190e
Chris Zambito <c.r.zambito@gmail.com>
2024-08-03 16:35:10 -0400

    fixed window bug w/ firebase, added routing for scan item and history on dashboard

---
+++ src/routes/(app)/dashboard/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import TopNav from "./components/navigation/TopNav.svelte";
+    import ScanItem from "./components/scan-item/ScanItem.svelte";
+    import History from "./components/history/History.svelte";
+    import { activeDashboardPage } from "$lib/firebase";
+</script>
+
+<TopNav />
+<ScanItem />
+
+{#if $activeDashboardPage === 'scan-item'}
+    <ScanItem />
+{:else if $activeDashboardPage === 'history'}
+    <History />
+{/if}
